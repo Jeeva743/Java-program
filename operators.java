2786a990aabcce030d4b2c6687eef232fc0fef84
Jeeva <jeeva.mc2006@gmail.com>
2025-09-04 08:34:10 +0530

    operator

---
+++ operators.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class operators {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int number =scan.nextInt();
+        if(number%3==0 || number%5==0)
+        {
+            System.out.println("the number is divisible by 3 and 5");
+        }
+        else{
+            System.out.println("the number is not divisible by 3 and 5");
+        }
+
+        
+
+    }
+    }
+ 
+    
+
